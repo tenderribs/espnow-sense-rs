ef81fcdb8114c7c785eb414fa5a71d21f03a5f7e
tenderribs <dc3dude@gmail.com>
2024-12-11 19:53:11 +0100

    send data using espnow

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,39 +1,54 @@
 #![no_std]
 #![no_main]
 
+use esp_alloc as _;
 use esp_backtrace as _;
-use esp_hal::delay::Delay;
-use esp_hal::prelude::*;
-use esp_hal::timer::timg::TimerGroup;
-use log::info;
-
-extern crate alloc;
+use esp_hal::{
+    prelude::*,
+    rng::Rng,
+    time::{self, Duration},
+    timer::timg::TimerGroup,
+};
+use esp_println::println;
+use esp_wifi::{esp_now::BROADCAST_ADDRESS, init};
 
 #[entry]
 fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
+
     let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
         config.cpu_clock = CpuClock::Clock80MHz; // underclock
         config
     });
 
-    esp_println::logger::init_logger_from_env();
-
     esp_alloc::heap_allocator!(72 * 1024);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let _init = esp_wifi::init(
+
+    let init = init(
         timg0.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
+        Rng::new(peripherals.RNG),
         peripherals.RADIO_CLK,
     )
     .unwrap();
 
-    let delay = Delay::new();
-    loop {
-        info!("Hello world!");
-        delay.delay(500.millis());
-    }
+    let wifi = peripherals.WIFI;
+    let mut esp_now = esp_wifi::esp_now::EspNow::new(&init, wifi).unwrap();
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
+    println!("esp-now version {}", esp_now.version().unwrap());
+
+    let mut next_send_time = time::now() + Duration::secs(5);
+    loop {
+        if time::now() >= next_send_time {
+            next_send_time = time::now() + Duration::secs(5);
+            println!("Send");
+
+            let status = esp_now
+                .send(&BROADCAST_ADDRESS, b"0123456789")
+                .unwrap()
+                .wait();
+            println!("Send broadcast status: {:?}", status)
+        }
+    }
 }
