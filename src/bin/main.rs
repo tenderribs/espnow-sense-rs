9624bf74d870ef53980183a95d35785c6f8c68a9
tenderribs <dc3dude@gmail.com>
2024-12-12 21:39:50 +0100

    add screen grabs of the oscilloscope

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,9 +4,10 @@
 use esp_alloc as _;
 use esp_backtrace as _;
 use esp_hal::{
+    gpio::{Level, Output},
     prelude::*,
     rng::Rng,
-    time::{self, Duration},
+    rtc_cntl::{sleep::TimerWakeupSource, Rtc},
     timer::timg::TimerGroup,
 };
 use esp_println::println;
@@ -14,17 +15,16 @@ use esp_wifi::{esp_now::BROADCAST_ADDRESS, init};
 
 #[entry]
 fn main() -> ! {
-    let start = time::now();
+    let start = esp_hal::time::now();
     esp_println::logger::init_logger_from_env();
 
     let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
-        config.cpu_clock = CpuClock::Clock80MHz; // underclock
+        config.cpu_clock = CpuClock::Clock160MHz; // underclock
         config
     });
 
     esp_alloc::heap_allocator!(72 * 1024);
-
     let timg0 = TimerGroup::new(peripherals.TIMG0);
 
     let init = init(
@@ -45,9 +45,15 @@ fn main() -> ! {
         .wait();
     println!("Send broadcast status: {:?}", status);
 
-    let duration: u64 = time::now().checked_duration_since(start).unwrap().ticks();
+    let duration: u64 = esp_hal::time::now()
+        .checked_duration_since(start)
+        .unwrap()
+        .ticks();
 
     println!("took {}ms", duration / 1000);
 
-    loop {}
+    // enter deep sleep;
+    let mut rtc = Rtc::new(peripherals.LPWR);
+    let src = TimerWakeupSource::new(core::time::Duration::from_secs(2));
+    rtc.sleep_deep(&[&src]);
 }
