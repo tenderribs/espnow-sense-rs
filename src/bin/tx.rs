e053aeb2d1601267a6c8d5e64808ce29770b0cac
tenderribs <dc3dude@gmail.com>
2024-12-20 11:26:03 +0100

    read temperature

--- src/bin/tx.rs
+++ src/bin/tx.rs
@@ -4,20 +4,20 @@
 use esp_alloc as _;
 use esp_backtrace as _;
 use esp_hal::{
+    delay::Delay,
     gpio::{Level, Output},
+    i2c::master::{Config, I2c},
     prelude::*,
     rng::Rng,
     rtc_cntl::{sleep::TimerWakeupSource, Rtc},
     timer::timg::TimerGroup,
 };
-use esp_println::println;
 use esp_wifi::esp_now::BROADCAST_ADDRESS;
+use sensirion_rht::{Addr, Device, Repeatability};
 
 use espnow_sense_rs::{set_rtc_time, DeepSleep};
 
-// https://sensirion.com/media/documents/1DA31AFD/65D613A8/Datasheet_STS3x_DIS.pdf
-
-// Log every 5 mins
+// default interval between measurements
 const SLEEP_DURATION_S: u64 = 5;
 
 // extended deep sleep config during overnight stop
@@ -43,18 +43,21 @@ const _: () = {
 fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
+    // init esp32
     let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
         config.cpu_clock = CpuClock::max();
 
         config
     });
-
-    let mut led = Output::new(peripherals.GPIO15, Level::High);
-
     esp_alloc::heap_allocator!(72 * 1024);
     let timg0 = TimerGroup::new(peripherals.TIMG0);
 
+    let delay = Delay::new();
+    let mut led = Output::new(peripherals.GPIO15, Level::High);
+    let mut tsensor_pwr = Output::new(peripherals.GPIO19, Level::High);
+
+    // prepare peripherals
     let init = esp_wifi::init(
         timg0.timer0,
         Rng::new(peripherals.RNG),
@@ -62,34 +65,42 @@ fn main() -> ! {
     )
     .unwrap();
 
-    // prepare peripherals
+    // init RTC
+    let rtc = Rtc::new(peripherals.LPWR);
+    set_rtc_time(&rtc);
+
+    // init esp-now
     let wifi = peripherals.WIFI;
     let mut esp_now = esp_wifi::esp_now::EspNow::new(&init, wifi).unwrap();
 
-    let mut rtc = Rtc::new(peripherals.LPWR);
+    // init temp sensor
+    // https://sensirion.com/media/documents/1DA31AFD/65D613A8/Datasheet_STS3x_DIS.pdf
+    let i2c = I2c::new(peripherals.I2C0, Config::default())
+        .with_sda(peripherals.GPIO0)
+        .with_scl(peripherals.GPIO1);
+    let mut tsensor = Device::new_sts3x(Addr::A, i2c, delay);
 
-    set_rtc_time(&rtc);
+    // read temperature value in single shot mode
+    if let Ok(temperature) = tsensor.single_shot(Repeatability::High) {
+        // broadcast the message
+        let _ = esp_now
+            .send(&BROADCAST_ADDRESS, &temperature.as_celsius().to_le_bytes())
+            .unwrap()
+            .wait();
+    }
 
-    // get the RTC time as a value to send onwards.
-    let now: u64 = rtc
-        .current_time()
-        .and_utc()
-        .timestamp_micros()
-        .try_into()
-        .expect("current_time is negative");
-
-    // broadcast the message
-    let status = esp_now
-        .send(&BROADCAST_ADDRESS, &now.to_le_bytes())
-        .unwrap()
-        .wait();
-    println!("Send broadcast status: {:?}", status);
-
+    // power down peripherals
     led.set_low();
+    tsensor_pwr.set_low();
 
-    // enter deep sleep;
+    enter_deep_sleep(rtc);
+    panic!("Entering deep sleep failed");
+}
+
+fn enter_deep_sleep(mut rtc: Rtc) {
     let ds = DeepSleep::new(BEDTIME_HR, WAKEUP_HR, UTC_DIFF, SLEEP_DURATION_S);
     let duration = ds.sleep_duration(rtc.current_time().time());
+
     let wake_src =
         TimerWakeupSource::new(core::time::Duration::from_secs(duration));
     rtc.sleep_deep(&[&wake_src]);
