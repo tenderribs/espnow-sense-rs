3d3f65f65ef965f05ace28eaf2d95272d4b2bc7f
tenderribs <dc3dude@gmail.com>
2024-12-22 11:45:32 +0100

    logging works

--- src/bin/rx.rs
+++ src/bin/rx.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+use alloc::format;
+use chrono::{Datelike, Timelike};
 use core::cell::RefCell;
 use embassy_executor::Spawner;
 use embedded_hal_bus::spi::RefCellDevice;
-use embedded_sdmmc::SdCard;
+use embedded_sdmmc::{Mode, SdCard, VolumeIdx, VolumeManager};
 use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
@@ -44,16 +46,6 @@ async fn main(_spawner: Spawner) {
 
     let delay = Delay::new();
 
-    // init esp-now
-    let init = esp_wifi::init(
-        timer1.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-    )
-    .unwrap();
-    let wifi = peripherals.WIFI;
-    let mut esp_now: EspNow = EspNow::new(&init, wifi).unwrap();
-
     // init SPI based on pinout: https://files.waveshare.com/wiki/ESP32-C6-LCD-1.47/ESP32-C6-LCD-1.47_schemetics.pdf
     let spi = Spi::new_with_config(
         peripherals.SPI2,
@@ -69,10 +61,33 @@ async fn main(_spawner: Spawner) {
     .with_cs(peripherals.GPIO4)
     .into_async();
 
+    // prepare SD card access as a device over a shared SPI bus
     // https://github.com/syrtcevvi/rust-esp32-embedded-sdmmc/blob/master/examples/esp-hal/src/main.rs
     let spi = RefCell::new(spi);
     let sd_spi_device = RefCellDevice::new(&spi, DummyCsPin, delay).unwrap();
     let sd_card = SdCard::new(sd_spi_device, delay);
+    info!("Instantiated SD card struct");
+
+    // waits until SD card is inserted
+    let mut volume_mgr = VolumeManager::new(sd_card, SdRtc::new(&rtc));
+    let mut volume0 = volume_mgr
+        .open_volume(VolumeIdx(0))
+        .expect("Can't open first partition on SD card");
+    let mut root_dir =
+        volume0.open_root_dir().expect("Cannot open root directory");
+    let mut file = root_dir
+        .open_file_in_dir("log.csv", Mode::ReadWriteCreateOrAppend)
+        .expect("Cannot open CSV file for appending");
+
+    // init esp-now
+    let init = esp_wifi::init(
+        timer1.timer0,
+        esp_hal::rng::Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .unwrap();
+    let wifi = peripherals.WIFI;
+    let mut esp_now: EspNow = EspNow::new(&init, wifi).unwrap();
 
     loop {
         let recv = esp_now.receive_async().await;
@@ -80,6 +95,17 @@ async fn main(_spawner: Spawner) {
         if let Ok(bytes) = recv.data().try_into() {
             let val = f64::from_le_bytes(bytes);
             info!("recv T meas {}", val);
+
+            // construct line
+            let timestamp = rtc.current_time().and_utc().to_rfc3339();
+            let line = format!("{}, {}\r\n", timestamp, val);
+
+            // append line
+            if let Ok(()) = file.seek_from_end(0) {
+                if let Ok(()) = file.write(line.as_bytes()) {
+                    file.flush().unwrap();
+                }
+            }
         }
     }
 }
@@ -99,5 +125,31 @@ impl embedded_hal::digital::OutputPin for DummyCsPin {
     #[inline(always)]
     fn set_high(&mut self) -> Result<(), Self::Error> {
         Ok(())
+    }
+}
+
+// shadow the Rtc struct to implement the TimeSource trait.
+struct SdRtc<'a> {
+    rtc: &'a Rtc<'a>,
+}
+
+impl<'a> SdRtc<'a> {
+    pub fn new(rtc: &'a Rtc<'a>) -> Self {
+        Self { rtc }
+    }
+}
+
+impl<'a> embedded_sdmmc::TimeSource for SdRtc<'a> {
+    fn get_timestamp(&self) -> embedded_sdmmc::Timestamp {
+        let now = self.rtc.current_time();
+
+        embedded_sdmmc::Timestamp {
+            year_since_1970: (now.year() - 1970).unsigned_abs() as u8,
+            zero_indexed_month: now.month().wrapping_sub(1) as u8,
+            zero_indexed_day: now.day().wrapping_sub(1) as u8,
+            hours: now.hour() as u8,
+            minutes: now.minute() as u8,
+            seconds: now.second() as u8,
+        }
     }
 }
