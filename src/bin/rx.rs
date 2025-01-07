a0bb83a95ef13374608ed51d7f5de6ad4631ccd7
tenderribs <dc3dude@gmail.com>
2025-01-07 20:18:35 +0100

    WIP

--- src/bin/rx.rs
+++ src/bin/rx.rs
@@ -22,20 +22,26 @@ use esp_hal::{
     rtc_cntl::Rtc,
     spi::{self, master::Spi, SpiMode},
 };
+use esp_println::println;
 use esp_wifi::esp_now::EspNow;
 use espnow_sense_rs::set_rx_rtc_time;
-use heapless::Vec;
+use heapless::{FnvIndexMap, Vec};
 use mipidsi::{
     models::ST7789,
     options::{ColorInversion, Orientation, Rotation},
     Builder,
 };
 
-static CHANNEL: MeasChannel = MeasChannel::new();
 extern crate alloc;
 
-// 54:32:04:33:6b:04
-// 54:32:04:33:69:90
+static CHANNEL: MeasChannel = MeasChannel::new();
+
+const TX_DEVS: [[u8; 6]; 3] = [
+    [0x54, 0x32, 0x04, 0x33, 0x69, 0x90], // 54:32:04:33:69:90
+    [0x54, 0x32, 0x04, 0x33, 0x6b, 0x04], // 54:32:04:33:6b:04
+    [0x54, 0x32, 0x04, 0x33, 0x66, 0x3c], // 54:32:04:33:66:3c
+];
+
 type MeasChannel = PubSubChannel<CriticalSectionRawMutex, Measurement, 4, 2, 1>;
 // type MeasSubscriber =
 //     Subscriber<'static, CriticalSectionRawMutex, Measurement, 4, 2, 1>;
@@ -60,11 +66,10 @@ async fn main(spawner: Spawner) {
     // init RTC
     let rtc = Rtc::new(peripherals.LPWR);
 
-    // I2C bus to access the external RTC
+    // access external RTC via I2c
     let i2c = I2c::new(peripherals.I2C0, i2c::master::Config::default())
         .with_scl(peripherals.GPIO0)
         .with_sda(peripherals.GPIO1);
-
     set_rx_rtc_time(&rtc, i2c);
 
     // init timers
@@ -92,29 +97,8 @@ async fn main(spawner: Spawner) {
     .into_async();
     let spi_bus = RefCell::new(spi_bus);
 
-    let lcd_spi_dev = RefCellDevice::new(&spi_bus, lcd_cs, delay).unwrap();
-    let di = SPIInterface::new(lcd_spi_dev, lcd_dc);
-    let mut display = Builder::new(ST7789, di)
-        .reset_pin(lcd_rst)
-        .invert_colors(ColorInversion::Normal)
-        .orientation(Orientation::default().rotate(Rotation::Deg270))
-        .init(&mut delay)
-        .expect("Cannot init display");
-    display.clear(Rgb565::WHITE).unwrap();
-
-    let sd_spi_device = RefCellDevice::new(&spi_bus, sd_cs, delay).unwrap(); // prepare SD card access as a device over a shared SPI bus via refcell
-    let sd_card = SdCard::new(sd_spi_device, delay); // open SD card when inserted
-    let mut volume_mgr = VolumeManager::new(sd_card, SdRtc::new(&rtc));
-
-    // open CSV file for writing
-    let mut volume0 = volume_mgr
-        .open_volume(VolumeIdx(0))
-        .expect("Can't open first partition on SD card");
-    let mut root_dir =
-        volume0.open_root_dir().expect("Cannot open root directory");
-    let mut file = root_dir
-        .open_file_in_dir("LOG.CSV", Mode::ReadWriteCreateOrAppend)
-        .expect("Cannot open CSV file for appending");
+    let _lcd_spi_dev = RefCellDevice::new(&spi_bus, lcd_cs, delay).unwrap();
+    let _sd_spi_device = RefCellDevice::new(&spi_bus, sd_cs, delay).unwrap(); // prepare SD card access as a device over a shared SPI bus via refcell
 
     // init esp-now
     let init = esp_wifi::init(
@@ -133,36 +117,78 @@ async fn main(spawner: Spawner) {
 
     loop {
         let recv = esp_now.receive_async().await;
-
-        if let Ok(bytes) = recv.data().try_into() {
-            let value = f32::from_le_bytes(bytes);
-
-            let meas = Measurement {
-                timestamp: rtc.current_time(),
-                src_addr: recv.info.src_address,
-                value,
-            };
-
-            pub0.publish_immediate(meas);
+        println!("received message");
+        // only process comms from whitelisted TX devices
+        if !TX_DEVS.iter().any(|&txdev| txdev == recv.info.src_address) {
+            println!("rejecting message");
+            continue;
         }
+
+        println!("proceeding with message");
+
+        // if let Ok(bytes) = recv.data().try_into() {
+        //     let value = f32::from_le_bytes(bytes);
+        //     println!("parsed bytes as f32");
+
+        //     let meas = Measurement {
+        //         timestamp: rtc.current_time(),
+        //         src_addr: recv.info.src_address,
+        //         value,
+        //     };
+
+        //     pub0.publish_immediate(meas);
+        // }
     }
 }
 
 #[embassy_executor::task]
 async fn display_task() {
-    let _measurements: Vec<Measurement, 50>;
+    println!("starting display task");
+    // let di = SPIInterface::new(lcd_spi_dev, lcd_dc);
+    // let mut display = Builder::new(ST7789, di)
+    //     .reset_pin(lcd_rst)
+    //     .invert_colors(ColorInversion::Normal)
+    //     .orientation(Orientation::default().rotate(Rotation::Deg270))
+    //     .init(&mut delay)
+    //     .expect("Cannot init display");
+    // display.clear(Rgb565::WHITE).unwrap();
+
+    type Points = Vec<Measurement, 50>;
+
+    // create hashmap to store 50 measurements per TX device
+    let mut ds: FnvIndexMap<[u8; 6], Points, 4> = FnvIndexMap::new();
+    assert!(TX_DEVS.len() < 4);
+    TX_DEVS.iter().for_each(|&txdev| {
+        ds.insert(txdev, Points::new()).unwrap();
+    });
 
     let mut sub = CHANNEL.subscriber().unwrap();
     loop {
         if let pubsub::WaitResult::Message(_meas) = sub.next_message().await {
             // process message
+            println!("received value in display task");
         }
     }
 }
 
 #[embassy_executor::task]
 async fn sd_card_task() {
+    println!("starting SD card task");
     let mut sub = CHANNEL.subscriber().unwrap();
+
+    // let sd_card = SdCard::new(sd_spi_device, delay); // open SD card when inserted
+    // let mut volume_mgr = VolumeManager::new(sd_card, SdRtc::new(&rtc));
+
+    // // open CSV file for writing
+    // let mut volume0 = volume_mgr
+    //     .open_volume(VolumeIdx(0))
+    //     .expect("Can't open first partition on SD card");
+    // let mut root_dir =
+    //     volume0.open_root_dir().expect("Cannot open root directory");
+    // let mut file = root_dir
+    //     .open_file_in_dir("LOG.CSV", Mode::ReadWriteCreateOrAppend)
+    //     .expect("Cannot open CSV file for appending");
+
     loop {
         if let pubsub::WaitResult::Message(meas) = sub.next_message().await {
             let src_addr = format!("{:02x?}", &meas.src_addr[..6]);
@@ -173,6 +199,8 @@ async fn sd_card_task() {
                 src_addr,
                 meas.value
             );
+
+            println!("received value in sd card task");
 
             // // append line
             // if let Ok(()) = file.seek_from_end(0) {
@@ -210,9 +238,23 @@ impl<'a> embedded_sdmmc::TimeSource for SdRtc<'a> {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Measurement {
     timestamp: NaiveDateTime,
     value: f32,
     src_addr: [u8; 6],
+}
+
+struct PlotPoint {
+    timestamp: NaiveDateTime,
+    value: f32,
+}
+
+impl From<Measurement> for PlotPoint {
+    fn from(m: Measurement) -> Self {
+        Self {
+            timestamp: m.timestamp,
+            value: m.value,
+        }
+    }
 }
